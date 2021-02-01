2bf809628ba99a9ad99517ecf32f86dd58e450dd
John Bourne <johnbourne@live.co.uk>
2021-02-01 22:25:17 +0000

    added search on keypress

--- script.js
+++ script.js
@@ -37,3 +37,13 @@ function searchfunction() {
    });
   
 }
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("searchbtn").click();
+  }
+});
