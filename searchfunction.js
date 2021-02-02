67b2104048708344a35a8c8f57ae852c23d16a1d
John Bourne <johnbourne@live.co.uk>
2021-02-02 15:42:18 +0000

    Update searchfunction.js

--- searchfunction.js
+++ searchfunction.js
@@ -2,8 +2,6 @@ function searchfunction() {
   $("#output").html("");
   $("#searchbtn").html("...");
   $("#searchbtn").attr("disabled", true);
-  $("#searchbtn").attr("background-color", #f2f2f2);
-  $("#searchbtn").attr("color", black);
   $("#pagination").html("");
   var offset = ""
   var baseid = "appoSpWy6YfbT6owy";
