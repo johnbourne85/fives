21108c73a772a020cb68114c9ba6a92b8bb1cb45
John Bourne <johnbourne@live.co.uk>
2021-02-02 08:18:12 +0000

    logging enter press

--- script.js
+++ script.js
@@ -22,13 +22,14 @@ function searchfunction() {
      } else {
       for (var i = 0; i < songData.records.length; i++) {
      var songitem = 
-     '<div class="card"><div class="song">'+songData.records[i].fields.title
+     '<div class="card"><div class="song">'
+     +songData.records[i].fields.title
      +'</div><div class="artist">by '
      + songData.records[i].fields.artistRoll
      +'</div><div class="who">Chosen by '+ songData.records[i].fields.whoSongRoll
      +' in "'+songData.records[i].fields.themeRoll 
      +'" week.</div>';
-        console.log(songitem)
+
         $("#output").append(songitem); 
           $("#searchbtn").html("Search")
         $("#searchbtn").attr("disabled", false);
@@ -44,6 +45,7 @@ input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Trigger the button element with a click
+    console.log("enter");
     document.getElementById("searchbtn").click();
   }
 });
