a51ba4f332c7c5f03cabc285871952b8baf04d38
John Bourne <johnbourne@live.co.uk>
2021-02-22 12:18:16 +0000

    changed themes page

--- themes.js
+++ themes.js
@@ -3,8 +3,21 @@ const baseid = "appoSpWy6YfbT6owy";
 const tablename = "themes";
 const apikey = "keyikCO7h5adp9Xtx";
 const searchURL = `https://api.airtable.com/v0/${baseid}/${tablename}?api_key=${apikey}`;
+console.log(searchURL);
 document.querySelector("#loading").classList.remove("hidden");
 let songList = "";
+
+const songsInTheme = function (songs, artists) {
+  const songsArr = songs.split(";;;");
+  const artistsArr = artists.split(";;;");
+  const songsAndArtist = [];
+
+  for (let i = 0; i < songsArr.length; i++) {
+    songsAndArtist.push([songsArr[i], artistsArr[i]]);
+  }
+
+  return songsAndArtist;
+};
 
 //Filter by person, or "all"
 function filterByPerson(chosenPerson) {
@@ -16,32 +29,88 @@ function filterByPerson(chosenPerson) {
   $.getJSON(searchURL, function (songData) {
     for (let i = 0; i < songData.records.length; i++) {
       if (chosenPerson == "All") {
-        let output = `
-        <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">  ${songData.records[i].fields.theme}</h5>
-        <p class="card-text">by ${songData.records[i].fields.whothemeName}</p>
-        <a href="${songData.records[i].fields.spotifyUrl}" class="btn btn-primary">Listen on Spotify</a>
+        let themeInfo = ` <div class="accordion-item">
+        <div class="accordion-header" id="heading${i + 1}">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapse${i + 1}"
+          aria-expanded="true"
+          aria-controls="collapse${i + 1}"
+        ><div class="totalArtistsChosenByUs">
+        <h3>${songData.records[i].fields.theme}</h3>
+        <a href="${
+          songData.records[i].fields.spotifyUrl
+        }" class="text-muted" id="totalArtists${i}">Listen on Spotify</a>
+      
       </div>
-    </div>
-    `;
+      </div>
+      <div
+        id="collapse${i + 1}"
+        class="accordion-collapse collapse"
+        aria-labelledby="heading${i + 1}"
+        data-bs-parent="#output"
+      >
+        <div class="accordion-body">
+          <ol id="songList${i}">
+          </ol>
+        </div>
+      </div>
+      </div>`;
+
+        $("#output").append(themeInfo);
+        document.querySelector("#loading").classList.add("hidden");
+
+        const songsArr = songData.records[i].fields.songsRoll.split(";;;");
+        const artistsArr = songData.records[i].fields.artistsRoll.split(";;;");
+
+        for (let j = 0; j < songsArr.length; j++) {
+          let songList = `<li>
+          <strong>${songsArr[j]}</strong> by ${artistsArr[j]}
+              </li>`;
+          $(`#songList${i}`).append(songList);
+        }
 
         //if person seletcted isn't the person in this part of the loop, skip it
       } else if (songData.records[i].fields.whothemeName != chosenPerson) {
         continue;
-      }
-      let output = `
-      <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">  ${songData.records[i].fields.theme}</h5>
-      <p class="card-text">by ${songData.records[i].fields.whothemeName}</p>
-      <a href="${songData.records[i].fields.spotifyUrl}" class="btn btn-primary">Listen on Spotify</a>
-    </div>
-  </div>
-  `;
+      } else if (chosenPerson != "All") {
+        let themeInfo = ` <div class="accordion-item">
+        <div class="accordion-header" id="heading${i + 1}">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapse${i + 1}"
+          aria-expanded="true"
+          aria-controls="collapse${i + 1}"
+        ><div class="totalArtistsChosenByUs">
+        <h3>${songData.records[i].fields.theme}</h3>
+        <a href="${
+          songData.records[i].fields.spotifyUrl
+        }" class="text-muted" id="totalArtists${i}">Listen on Spotify</a>
+      
+      </div>
+      </div>
+      <div
+        id="collapse${i + 1}"
+        class="accordion-collapse collapse"
+        aria-labelledby="heading${i + 1}"
+        data-bs-parent="#output"
+      >
+        <div class="accordion-body">
+          <ol id="songList${i}">
+          </ol>
+        </div>
+      </div>
+      </div>`;
 
-      $("#output").append(output);
-      document.querySelector("#loading").classList.add("hidden");
+        $("#output").append(themeInfo);
+        document.querySelector("#loading").classList.add("hidden");
+
+        songsInTheme();
+      }
     }
   });
 }
